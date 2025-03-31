834ecee5afb4cb599ee75dc2398dd910a8504350
Davi Kirchmaier Paiva <104040768+KirchmaierDavi@users.noreply.github.com>
2025-03-31 17:17:24 -0300

    Iniciando repositorio para realização do projeto

---
+++ demo/src/main/java/com/integrationapi/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.integrationapi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
